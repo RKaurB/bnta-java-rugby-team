1caa39d7dd98521c9b0978957faf2eedf12d87ba
Rachel Kaur <rachel.km@outlook.com>
2022-02-17 23:32:52 +0000

    Add properties, constructor, getters & setters

--- src/main/java/pojos/Manager.java
+++ src/main/java/pojos/Manager.java
@@ -1,4 +1,50 @@
 package pojos;
 
+import java.util.Objects;
+
 public class Manager {
+
+    // Properties
+    private String name;
+
+    // Default constructor
+    public Manager() {
+
+    }
+
+    // Constructor
+    public Manager(String name) {
+        this.name = name;
+    }
+
+    // Getters & Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    // Equals & hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Manager manager = (Manager) o;
+        return Objects.equals(name, manager.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
