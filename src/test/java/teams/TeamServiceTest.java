d64044aea978c4756bf8c6fe62790172b6613bec
Rachel Kaur <rachel.km@outlook.com>
2022-02-19 23:26:36 +0000

    add canCountEmptySpacesOnTeamWithOnePlayer test

--- src/test/java/teams/TeamServiceTest.java
+++ src/test/java/teams/TeamServiceTest.java
@@ -33,4 +33,28 @@ public class TeamServiceTest {
         assertThat(actual).isEqualTo(expected);
     }
 
+    // @Test flags up to JUnit that this is to be run as a test
+    @Test
+    // Test that it gives us 14 spaces when one player has been added
+    public void canCountEmptySpacesOnTeamWithOnePlayer() {
+        // GIVEN
+        Manager manager = new Manager("test manager");
+        Team team = new Team("test team", manager);
+        // This time, we need a player
+        Player player = new Player("test player", "winger", 11, 2);
+        TeamService underTest = new TeamService();
+        // Test if addPlayerToTeam throws an exception - wrap in a try/catch block (may not be best practice)
+        try {
+            underTest.addPlayerToTeam(team, player);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // WHEN
+        // (The When should typically be one line, i.e. we're doing one thing at a time when we're unit testing
+        int actual = underTest.countEmptySpacesOnTeam(team);
+        // THEN
+        int expected = 14;
+        assertThat(actual).isEqualTo(expected);
+    }
+
 }
