fd9931b3b0cd7b78e37825b372d79b66438017c6
Rachel Kaur <rachel.km@outlook.com>
2022-02-19 20:11:13 +0000

    add comments and test addPlayerToTeam method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,55 @@
+// IMPORTS
 import managers.Manager;
 import players.Player;
 import teams.Team;
 import teams.TeamService;
 
-// Main class
+// MAIN CLASS
 public class Main {
 
-    // Main method
+    // MAIN METHOD
     public static void main(String[] args) {
 
-        // Create new instance of player object
+        // CREATE NEW INSTANCE OF PLAYER OBJECT
         Player player = new Player("Finn Russell", "Fly Half", 10, 10);
 
-        // Create new instance of manager object
+        // CREATE NEW INSTANCE OF MANAGER OBJECT
         Manager manager1 = new Manager("Gregor Townsend");
 
-        // Create new instance of team object
+        // CREATE NEW INSTANCE OF TEAM OBJECT
         Team scotland = new Team("Scotland", manager1);
 
-        // Create another instance of manager object
+        // CREATE ANOTHER INSTANCE OF MANAGER OBJECT
         Manager manager2 = new Manager("Eddie Jones");
 
-        // Create another instance of team object
+        // CREATE ANOTHER INSTANCE OF TEAM OBJECT
         Team england = new Team("England", manager2);
 
-        // Make new instance of Team Service
+        // PRINT PLAYER PROPERTIES
+        System.out.println(player);
+        // System.out.println(player.toString());
+
+        // PRINT TEAM PROPERTIES
+        System.out.println("Scotland: " + scotland);
+        // System.out.println(scotland.toString());
+        System.out.println("England: " + england);
+        // System.out.println(england.toString());
+
+        // MAKE NEW INSTANCE OF TEAM SERVICE
         TeamService teamService = new TeamService();
         // Test countyEmptySpacesOnTeam method
         // County number of empty spaces in Scotland team
         int spaces = teamService.countEmptySpacesOnTeam(scotland);
-        System.out.println(spaces);
+        System.out.println("Empty spaces on team (Scotland): " + spaces);
 
-        // Print player properties
-         System.out.println(player);
-        // System.out.println(player.toString());
-
-        // Print team properties
-        System.out.println(scotland);
-        // System.out.println(scotland.toString());
-        System.out.println(england);
-        // System.out.println(england.toString());
-
-        // Print players array
-
+        // PRINT NUMBER OF EMPTY SPACES ON SCOTLAND TEAM BEFORE ADDING A PLAYER
+        int spacesBefore = teamService.countEmptySpacesOnTeam(scotland);
+        System.out.println("Empty spaces on Scotland team before adding player: " + spacesBefore);
+        // ADD PLAYER TO SCOTLAND TEAM (USING THE ADDPLAYERTOTEAM METHOD IN TEAM SERVICE)
+        teamService.addPlayerToTeam(scotland, player);
+        // PRINT NUMBER OF EMPTY SPACES ON SCOTLAND TEAM AFTER ADDING A PLAYER
+        int spacesAfter = teamService.countEmptySpacesOnTeam(scotland);
+        System.out.println("Empty spaces on Scotland team after adding player: " + spacesAfter);
 
     }
 }
